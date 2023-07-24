321b8b68cde5ffd4041491175a9316d14e8cee5e
Oybek <programmeroybek211@gmail.com>
2023-07-24 14:10:25 +0500

    finished is react shop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
 import React from 'react'
-
+import '../App.css'
 export default function Footer() {
     return (
-        <footer class="page-footer">
-            <div class="footer-copyright">
-                <div class="container">
-                    © {new Date().getFullYear()} Copyright Text
-                    <a class="grey-text text-lighten-4 right" href="#!">React Shop</a>
+        <footer className="page-footer footerColor">
+            <div className="container">
+                <div className="row">
+                    <div className="col l6 s12">
+                        <h5 className="white-text">About is Bek Shop </h5>
+                        <p className="grey-text text-lighten-4">
+                            In our online store, you can order any products you want
+                        </p>
+                    </div>
+                    <div className="col l4 offset-l2 s12">
+                        <h5 className="white-text">What can you do with us</h5>
+                        <ul>
+                            <li><a className="grey-text text-lighten-3" href="#!">Home</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Brend</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">About</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Bek shop uzbekistan
+                    <a className="grey-text text-lighten-4 right" href="#!">Bek Shop</a>
                 </div>
             </div>
         </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
-
+import foto from '../photo/logo.png'
+import '../App.css'
 export default function Navbar() {
     return (
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" className="brand-logo"><i class="material-icons">mood</i>React shop</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="collapsible.html">OnlineShop</a></li>
+        <nav className="navbarStyle">
+            <div className="nav-wrapper">
+                    <img src={foto} className='LogoNav' alt='LogoSHop' />
+                <a href="ds" className="brand-logo">
+                    Bek Shop
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="home">Home</a></li>
+                    <li><a href="home">Brend</a></li>
+                    <li><a href="home">About</a></li>
+                    <li><a href="home">Contact</a></li>
                 </ul>
             </div>
         </nav>
